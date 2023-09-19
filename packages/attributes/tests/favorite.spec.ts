541e5c84cf1f10dac46e3fc313b1bef7b68010a0
Dovbaka <dovbaka99@gmail.com>
2023-09-20 02:52:36 +0300

    Added favorite test

---
+++ packages/attributes/tests/favorite.spec.ts
@@ -0,0 +1,41 @@
+import { expect, test } from '@playwright/test';
+
+import { waitAttributeLoaded } from './utils';
+
+test.beforeEach(async ({ page }) => {
+  await page.goto('https://dev-attributes-favorite.webflow.io/');
+});
+
+test.describe('favorite', () => {
+  test('Loader should be hidden', async ({ page }) => {
+    await waitAttributeLoaded(page, 'favorite');
+
+    const loader = page.getByTestId('loader');
+
+    const isHidden = await loader.evaluate((element) => {
+      const computedStyle = getComputedStyle(element);
+      return computedStyle.display === 'none';
+    });
+
+    expect(isHidden).toBe(true);
+  });
+
+  test('List should be empty', async ({ page }) => {
+    await waitAttributeLoaded(page, 'favorite');
+
+    const list = page.getByTestId('list-favorite');
+
+    const childCount = await list.evaluate((element) => element.children.length);
+    expect(childCount).toBe(0);
+  });
+
+  test('Like adds element to store', async ({ page }) => {
+    await waitAttributeLoaded(page, 'favorite');
+
+    const likeButton = page.getByTestId('like');
+
+    await likeButton.nth(0).click();
+
+    await expect(likeButton.nth(0)).toHaveClass(/is-active/);
+  });
+});
