f4de869993af056982b445ed9b60065e32460226
Dovbaka <dovbaka99@gmail.com>
2023-10-10 14:43:51 +0300

    Fixed nested category

--- packages/favorite/src/actions/add.ts
+++ packages/favorite/src/actions/add.ts
@@ -18,7 +18,7 @@ export const addItemToList = async (
   const itemDocument = await fetchPageDocument(link);
   if (itemDocument) {
     const elementsWithData = itemDocument.querySelectorAll('[fs-favorite-field]');
-    const elementData = {} as { [key: string]: string };
+    const elementData = {} as { [key: string]: string[] };
     elementsWithData.forEach((element) => {
       const attributeValue = element.getAttribute('fs-favorite-field');
       if (!attributeValue) return;
@@ -28,29 +28,34 @@ export const addItemToList = async (
       } else {
         content = element.textContent?.trim() || '';
       }
-      elementData[attributeValue] = content;
+      if (!elementData[attributeValue]) {
+        elementData[attributeValue] = [content];
+      } else {
+        elementData[attributeValue].push(content);
+      }
     });
 
     if (itemTemplate) {
       const elementCopy = itemTemplate.cloneNode(true) as HTMLElement;
       const fieldsWithContent = elementCopy.querySelectorAll('[fs-favorite-field]');
+      console.log(fieldsWithContent);
       const removeButton = queryElement('remove', { scope: elementCopy });
       fieldsWithContent.forEach((element) => {
         const fieldName = element.getAttribute('fs-favorite-field');
         if (!fieldName) return;
         switch (element.tagName.toLowerCase()) {
           case 'img':
-            element.setAttribute('srcset', elementData[fieldName]);
-            element.setAttribute('src', elementData[fieldName]);
+            element.setAttribute('srcset', elementData[fieldName][0]);
+            element.setAttribute('src', elementData[fieldName][0]);
             break;
 
           case 'a':
             element.setAttribute('href', link);
-            element.textContent = elementData[fieldName] || '';
+            element.textContent = elementData[fieldName][0] || '';
             break;
 
           default:
-            element.textContent = elementData[fieldName] || '';
+            element.innerHTML = elementData[fieldName]?.map((item) => item.trim()).join('<br>') || 'No items found';
             break;
         }
       });
