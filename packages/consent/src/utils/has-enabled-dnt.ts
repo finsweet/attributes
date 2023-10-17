9e69e192b50406d7d82d62dfb5717b6a549ace78
Robert Kibet <kipronokrobert@gmail.com>
2023-10-17 10:28:23 +0300

    feat: update dnt checker utility logic
    article link: https://schalkneethling.medium.com/respect-user-choice-do-not-track-6c13b805b256

--- packages/consent/src/utils/has-enabled-dnt.ts
+++ packages/consent/src/utils/has-enabled-dnt.ts
@@ -8,27 +8,57 @@ interface CustomNavigator extends Navigator {
  * @returns {boolean} Returns `true` if the user prefers not to be tracked, or `false` if they allow tracking
  */
 export const hasEnabledDNT = (): boolean => {
+  'use strict';
+
   const customNavigator: CustomNavigator = navigator;
-  //solution ref: https://www.cogmentis.com/how-to-properly-check-for-do-not-track-with-javascript/
+  //solution ref: https://schalkneethling.medium.com/respect-user-choice-do-not-track-6c13b805b256
 
-  const dnt =
-    typeof customNavigator.doNotTrack !== 'undefined'
-      ? customNavigator.doNotTrack
-      : typeof window.doNotTrack !== 'undefined'
-      ? window.doNotTrack
-      : typeof customNavigator.msDoNotTrack !== 'undefined'
-      ? customNavigator.msDoNotTrack
-      : null;
+  /**
+   * Returns true or false based on whether doNotTack is enabled. It also takes into account the
+   * anomalies, such as !bugzilla 887703, which effect versions of Fx 31 and lower. It also handles
+   * IE versions on Windows 7, 8 and 8.1, where the DNT implementation does not honor the spec.
+   * @see https://bugzilla.mozilla.org/show_bug.cgi?id=1217896 for more details
+   * @params {string} [dnt] - An optional mock doNotTrack string to ease unit testing.
+   * @params {string} [userAgent] - An optional mock userAgent string to ease unit testing.
+   * @returns {boolean} true if enabled else false
+   */
 
-  if (!dnt) return false;
+  // for old version of IE we need to use the msDoNotTrack property of navigator
+  // on newer versions, and newer platforms, this is doNotTrack but, on the window object
+  // Safari also exposes the property on the window object.
+  let dntStatus = customNavigator.doNotTrack || window.doNotTrack || customNavigator.msDoNotTrack;
+  const ua = customNavigator.userAgent;
 
-  const dntValue = parseInt(dnt, 10);
+  // List of Windows versions known to not implement DNT according to the standard.
+  const anomalousWinVersions = ['Windows NT 6.1', 'Windows NT 6.2', 'Windows NT 6.3'];
 
-  if (dntValue === 1 || dnt === 'yes') {
-    // The user prefers NOT TO BE TRACKED on this request
-    return true;
+  const fxMatch = ua.match(/Firefox\/(\d+)/);
+  const ieRegEx = /MSIE|Trident/i;
+  const isIE = ieRegEx.test(ua);
+  // Matches from Windows up to the first occurance of ; un-greedily
+  // http://www.regexr.com/3c2el
+  const platform = ua.match(/Windows.+?(?=;)/g);
+
+  // With old versions of IE, DNT did not exist so we simply return false;
+  if (isIE && typeof Array.prototype.indexOf !== 'function') {
+    return false;
   }
 
-  // The user prefers to ALLOW TRACKING on this request
-  return false;
+  if (fxMatch && parseInt(fxMatch[1], 10) < 32) {
+    // Can't say for sure if it is 1 or 0, due to Fx bug 887703
+    dntStatus = 'Unspecified';
+  } else if (isIE && platform && anomalousWinVersions.indexOf(platform.toString()) !== -1) {
+    // default is on, which does not honor the specification
+    dntStatus = 'Unspecified';
+  } else {
+    // sets dntStatus to Disabled or Enabled based on the value returned by the browser.
+    // If dntStatus is undefined, it will be set to Unspecified
+    if (!dntStatus) {
+      dntStatus = 'Unspecified';
+    }
+
+    dntStatus = { '0': 'Disabled', '1': 'Enabled' }[dntStatus] || 'Unspecified';
+  }
+
+  return dntStatus === 'Enabled' ? true : false;
 };
