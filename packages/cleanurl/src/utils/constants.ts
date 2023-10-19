db118920dd528ee4297eaaa9275fb06eeec18ee7
Michael Gatuma <50529359+michaelgatuma@users.noreply.github.com>
2023-10-19 11:09:14 +0300

    feat: define SETTINGS in constants

--- packages/cleanurl/src/utils/constants.ts
+++ packages/cleanurl/src/utils/constants.ts
@@ -2,4 +2,19 @@ import { type AttributeElements, type AttributeSettings } from '@finsweet/attrib
 
 export const ELEMENTS = [] as const satisfies AttributeElements;
 
-export const SETTINGS = {} as const satisfies AttributeSettings;
+export const SETTINGS = {
+  query: {
+    key: 'query',
+    values: {
+      enable: 'enable',
+      disable: 'disable',
+    },
+  },
+  hash: {
+    key: 'hash',
+    values: {
+      enable: 'enable',
+      disable: 'disable',
+    },
+  },
+} as const satisfies AttributeSettings;
