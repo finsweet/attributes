b551adc17c4cdbed402f09ef9f1a55a9eebabc65
Robert Kibet <kipronokrobert@gmail.com>
2023-10-25 12:53:49 +0300

    test: readtime

--- packages/attributes/tests/readtime.spec.ts
+++ packages/attributes/tests/readtime.spec.ts
@@ -1,19 +1,44 @@
+import { formatNumberToLocale, parseNumericAttribute } from '@finsweet/attributes-utils';
 import { expect, test } from '@playwright/test';
 
 import { waitAttributeLoaded } from './utils';
 
-test.beforeEach(async ({ page }) => {
+const DEFAULT_WPM = 265;
+const DEFAULT_DECIMALS = 0;
+const DEFAULT_LOCALE = 'auto';
+
+test.beforeEach(async ({ page, browser }) => {
+  // set locale to es-ES
+  await browser.newContext({
+    locale: 'es-ES',
+  });
+
   await page.goto('http://fs-attributes.webflow.io/readtime');
 });
 
 test.describe('readtime', () => {
-  test('Displays the read time', async ({ page }) => {
+  test('Displays the read time for each instance', async ({ page }) => {
     await waitAttributeLoaded(page, 'readtime');
 
-    const timeElement = page.locator('[fs-readtime-element="time"]');
-    await expect(timeElement).toHaveText('4.2');
+    const elements = await page.locator('[fs-readtime-element="time"][fs-readtime-instance]').all();
 
-    const timeElement2 = page.locator('[fs-readtime-element="time-2"]');
-    await expect(timeElement2).toHaveText('1.0');
+    for (const element of elements) {
+      const readtime = await element.textContent();
+
+      expect(readtime).toBeTruthy();
+
+      expect(readtime?.length).toBeGreaterThan(2);
+    }
+  });
+
+  test('Displays the read time in locality', async ({ page }) => {
+    await waitAttributeLoaded(page, 'readtime');
+
+    // test es-ES locale
+    const element = await page.locator('[fs-readtime-element="time"][fs-readtime-locale="es-ES"]');
+
+    const readtime = await element.textContent();
+
+    expect(readtime).toBe('1,0 minuto');
   });
 });
