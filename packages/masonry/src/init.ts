84807c1f784d1eecc658add919d3def1997b5801
Dovbaka <dovbaka99@gmail.com>
2023-10-31 11:57:31 +0200

    Added masonry layout updates on screen resize

--- packages/masonry/src/init.ts
+++ packages/masonry/src/init.ts
@@ -3,20 +3,58 @@ import { getCollectionElements } from '@finsweet/attributes-utils';
 
 import { initMasonryLayout } from './actions/masonry';
 import { queryAllElements } from './utils';
+import { preloadImages } from './utils';
 
 /**
  * Inits masonry functionality.
  */
 export const init: FsAttributeInit = async () => {
+  const originalStates = new Map();
+
   await waitWebflowReady();
 
   const listReferences = queryAllElements('list');
+  let resizeTimeout;
+
+  // Reset function to restore the original state
+  const resetToOriginalState = (element) => {
+    const originalState = originalStates.get(element);
+    if (originalState) {
+      element.innerHTML = originalState.innerHTML;
+    }
+  };
 
   for (const listReference of listReferences) {
     const listElement = getCollectionElements(listReference, 'list') || listReference;
+
+    // Store the original state of the element
+    originalStates.set(listElement, {
+      innerHTML: listElement.innerHTML,
+    });
+
     listElement.style.position = 'relative';
-    initMasonryLayout(listElement);
+
+    preloadImages(listElement, () => {
+      initMasonryLayout(listElement);
+    });
   }
+
+  const handleResize = () => {
+    clearTimeout(resizeTimeout);
+    resizeTimeout = setTimeout(() => {
+      for (const listReference of listReferences) {
+        const listElement = getCollectionElements(listReference, 'list') || listReference;
+
+        resetToOriginalState(listElement);
+
+        preloadImages(listElement, () => {
+          initMasonryLayout(listElement);
+        });
+      }
+    }, 500);
+  };
+
+  window.addEventListener('resize', handleResize);
 
   return {
     result: listReferences,
---
+++ packages/masonry/src/utils/helpers.ts
@@ -0,0 +1,41 @@
+/**
+ * Function to preload all images within an element with a timeout.
+ * @param element - The HTML element to search for images within.
+ * @param callback - The callback function to be called when all images are loaded or when the timeout is reached.
+ */
+export function preloadImages(element, callback) {
+  const images = element.querySelectorAll('img');
+  const imageCount = images.length;
+
+  if (imageCount === 0) {
+    callback();
+    return;
+  }
+
+  const timeoutDuration = 6000; // Timeout for images to load
+  let loadedImages = 0;
+  let isCallbackCalled = false;
+
+  const handleTimeout = () => {
+    if (!isCallbackCalled) {
+      isCallbackCalled = true;
+      callback();
+    }
+  };
+
+  const timeoutId = setTimeout(handleTimeout, timeoutDuration);
+
+  images.forEach((image) => {
+    image.onload = () => {
+      loadedImages = loadedImages + 1;
+      if (loadedImages === imageCount) {
+        clearTimeout(timeoutId);
+        if (!isCallbackCalled) {
+          isCallbackCalled = true;
+          callback();
+        }
+      }
+    };
+    image.src = image.src; // Trigger the image to load
+  });
+}
