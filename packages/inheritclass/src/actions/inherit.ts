47951c2eee9963e88eeea7490d4e663e7d9af307
Robert Kibet <kipronokrobert@gmail.com>
2024-01-02 17:41:57 +0300

    fix: refactor init inheritclass attribute with todo

--- packages/inheritclass/src/actions/inherit.ts
+++ packages/inheritclass/src/actions/inherit.ts
@@ -1,38 +1,50 @@
-import { getAttribute, getInstanceIndex, getSettingSelector } from '../utils';
+import { getAttribute, getSettingSelector } from '../utils';
+
+/**
+ * Custom logic function to determine if a class should trigger inheritance.
+ * @param classValue The class value to be checked.
+ * @returns A boolean indicating if the classValue is a trigger class.
+ */
+const checkAddedAttribute = (classValue: string): boolean => {
+  console.log('classValue', classValue);
+
+  // todo: check other webflow w- or w-- classes?
+  return classValue.includes('w--current');
+};
 
 /**
  * Initializes a mutation observer to dynamically inherit classes to specified elements based on conditions.
- * @param parentElement - The parent element to which the mutation observer will be attached.
+ * @param parentElement The parent element to which the mutation observer will be attached.
  * @returns An instance of MutationObserver used to monitor changes in attributes.
  */
-export const initInheritClass = (parentElement: HTMLElement) => {
-  const inheritClassSelector = getSettingSelector('class');
-  const triggerClass = getAttribute(parentElement, 'listener');
-  if (!triggerClass) return;
-  const inheritElements = document.querySelectorAll(inheritClassSelector);
-  const instanceIndex = getInstanceIndex(parentElement);
+export const initInheritClass = (parentElement: HTMLElement): MutationObserver => {
+  const inheritElements = parentElement.querySelectorAll(getSettingSelector('class'));
 
   const observer = new MutationObserver((mutationsList) => {
     for (const mutation of mutationsList) {
       if (mutation.type === 'attributes' && mutation.attributeName === 'class') {
-        if (parentElement.classList.contains(triggerClass)) {
+        const currentClassValue = parentElement.getAttribute('class') || '';
+        const triggerClass = checkAddedAttribute(currentClassValue);
+
+        if (triggerClass) {
           inheritElements.forEach((childElement) => {
-            if (!(Number(getAttribute(childElement, 'instance')) === instanceIndex)) return;
             const inheritClass = getAttribute(childElement, 'class');
+
             if (inheritClass && !childElement.classList.contains(inheritClass)) {
               childElement.classList.add(inheritClass);
             }
           });
-        } else {
-          // Remove class from all child elements
-          inheritElements.forEach((childElement) => {
-            if (!(Number(getAttribute(childElement, 'instance')) === instanceIndex)) return;
-            const inheritClass = getAttribute(childElement, 'class');
-            if (inheritClass && childElement.classList.contains(inheritClass)) {
-              childElement.classList.remove(inheritClass);
-            }
-          });
+          return;
         }
+
+        // Remove class from all child elements
+        inheritElements.forEach((childElement) => {
+          const inheritClass = getAttribute(childElement, 'class');
+
+          if (inheritClass && childElement.classList.contains(inheritClass)) {
+            childElement.classList.remove(inheritClass);
+          }
+        });
       }
     }
   });
--- packages/inheritclass/src/init.ts
+++ packages/inheritclass/src/init.ts
@@ -1,17 +1,15 @@
 import { type FsAttributeInit, waitWebflowReady } from '@finsweet/attributes-utils';
 
 import { initInheritClass } from './actions/inherit';
-import { getSettingSelector } from './utils';
+import { queryAllElements } from './utils';
 
 /**
  * Inits the inheritclass attribute.
  */
 export const init: FsAttributeInit = async () => {
   await waitWebflowReady();
-  const listenerSelector = getSettingSelector('listener');
-  const parentElements = Array.from(document.querySelectorAll<HTMLElement>(listenerSelector));
+  const parentElements = queryAllElements('listener');
 
-  parentElements.forEach((element) => initInheritClass(element));
   const inheritInstances = parentElements.map((list) => initInheritClass(list));
 
   return {
