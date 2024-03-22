9f95b4c337aac79e87708b53823e39efa5b44176
Robert Kibet <kipronokrobert@gmail.com>
2024-03-22 16:01:59 +0300

    fix: lint issues

--- packages/tooltip/src/utils/selectors.ts
+++ packages/tooltip/src/utils/selectors.ts
@@ -2,7 +2,7 @@ import { generateSelectors, TOOLTIP_ATTRIBUTE } from '@finsweet/attributes-utils
 
 import { ELEMENTS, SETTINGS } from './constants';
 
-export const { getAttribute, getInstanceIndex, queryElement, getElementSelector, queryAllElements } = generateSelectors(
+export const { getAttribute, getInstance, queryElement, getElementSelector, queryAllElements } = generateSelectors(
   TOOLTIP_ATTRIBUTE,
   ELEMENTS,
   SETTINGS
