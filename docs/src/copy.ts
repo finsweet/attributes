763d22945def4d9a711abc70cd876415cbe3dc30
Robert Kibet <kipronokrobert@gmail.com>
2024-05-07 18:18:25 +0300

    fix: refactor copy utility script with more jsdoc and clarity

--- docs/src/copy.ts
+++ docs/src/copy.ts
@@ -1,10 +1,24 @@
-export const elements = [
+interface Configurator {
+  target: string;
+  text: string;
+  textContent: boolean;
+  script?: boolean;
+  stopPropagation: boolean;
+  button?: boolean;
+  changeText?: boolean;
+  detached?: boolean;
+}
+/**
+ * Copy Elements Configuration
+ * TODO: migrate this to attribute when ready.
+ */
+export const configurator: Configurator[] = [
   // script tag
   {
     target: '.copy_button',
     text: '.docs-2_script_richtext p',
     textContent: true,
-    format: true,
+    script: true,
     stopPropagation: false,
     button: true,
     changeText: true,
@@ -13,7 +27,7 @@ export const elements = [
     target: '.docs-2_flashcard-header-component .copy_button-dark',
     text: '.docs-2_script_richtext p',
     textContent: true,
-    format: true,
+    script: true,
     stopPropagation: false,
     detached: true,
     button: true,
@@ -24,7 +38,6 @@ export const elements = [
     target: '.panel_attributes_copy',
     text: '.panel_attributes_value',
     textContent: true,
-    format: false,
     stopPropagation: false,
     changeText: true,
   },
@@ -33,39 +46,19 @@ export const elements = [
     target: '.docs-2_tag_item .docs-2_tag_block',
     text: '',
     textContent: true,
-    format: false,
     stopPropagation: true,
   },
   {
     target: '.docs-2_tag_item .docs-2_tag_block',
     text: '',
     textContent: true,
-    format: false,
     stopPropagation: true,
   },
 ];
 
 /**
- * Reformats a script tag so that each attribute is on a new line.
- * @param {string} script - The script tag as a string.
- * @returns {string} The reformatted script tag.
+ * CSS string to be injected into the head of the document.
  */
-const reformatScriptTag = (script: string): string => {
-  return script.replace(/(<script|\s)(.*?)\s(.*?=".*?")/gs, '$1$2\n$3').replace(/\s>/, '\n>');
-};
-
-/**
- * Injects the specified CSS into the head of the document.
- * @param cssString - The CSS string to be injected.
- */
-const injectCSS = (cssString: string): void => {
-  const style = document.createElement('style');
-  style.type = 'text/css';
-  style.innerHTML = cssString;
-  document.head.appendChild(style);
-};
-
-// Define the CSS string
 const cssString = `
 .copied-text::before {
   content: "";
@@ -96,158 +89,247 @@ const cssString = `
 }
 `;
 
-const enableElement = (element: HTMLElement, textAlt = '') => {
-  const originalText = element.textContent;
+/**
+ * Injects the specified CSS into the head of the document.
+ * @param cssString - The CSS string to be injected.
+ */
+const injectCSS = (cssString: string): void => {
+  const style = document.createElement('style');
 
-  // query button_text element
+  style.setAttribute('type', 'text/css');
+  style.innerHTML = cssString;
 
-  if (textAlt) {
-    const button_text = element.querySelector('.button_text');
-    if (button_text) button_text.textContent = textAlt;
-    else element.textContent = textAlt;
-
-    setTimeout(() => {
-      if (button_text) button_text.textContent = originalText;
-      else element.textContent = originalText;
-      element.style.pointerEvents = 'auto';
-    }, 1000);
-  } else {
-    element.style.pointerEvents = 'auto';
-    element.classList.add('copied-visible');
-
-    setTimeout(() => {
-      element.classList.remove('copied-visible');
-      element.style.pointerEvents = 'auto';
-    }, 1000);
-  }
+  document.head.appendChild(style);
 };
+
+/**
+ * Enable an element by setting its pointer events to auto and toggle the copied text.
+ * @param element - The element to enable.
+ * @param textToShow - The text to show after copying.
+ * @returns
+ */
+const enableElement = (element: HTMLElement, textToShow = '') => {
+  const { textContent } = element;
+
+  if (textToShow) {
+    const button_text = element.querySelector('.button_text');
+
+    if (button_text) button_text.textContent = textToShow;
+    else element.textContent = textToShow;
+
+    setTimeout(() => {
+      if (button_text) button_text.textContent = textContent;
+      else element.textContent = textContent;
+
+      element.style.pointerEvents = 'auto';
+    }, 1000);
+
+    return;
+  }
+
+  element.style.pointerEvents = 'auto';
+  element.classList.add('copied-visible');
+
+  setTimeout(() => {
+    element.classList.remove('copied-visible');
+    element.style.pointerEvents = 'auto';
+  }, 1000);
+};
+
+/**
+ * Disable an element by setting its pointer events to none.
+ * @param element - The element to disable.
+ */
 const disableElement = (element: HTMLElement) => {
   element.style.pointerEvents = 'none';
 };
 
-const copyToClipboard = (textToCopy: string, element: HTMLElement, textAlt = '', textOnly?: boolean) => {
-  if (navigator.clipboard && textToCopy) {
-    disableElement(element);
+/**
+ * Copy the text to the clipboard.
+ * @param text - The text to copy.
+ * @param element - The element to enable after copying.
+ * @param textToShow - The text to show after copying.
+ */
+const copy = (text: string, element: HTMLElement, textToShow = '') => {
+  navigator.clipboard
+    .writeText(text)
+    .then(() => {
+      enableElement(element, textToShow);
+    })
+    .catch((err) => {
+      console.error('Failed to copy text: ', err);
+      enableElement(element, textToShow);
+    });
+};
 
-    const text = `<!-- Finsweet Attributes -->\n<script async type="module"${textToCopy}\n></script>`;
-    const copyText = textOnly ? textToCopy : text;
-    navigator.clipboard
-      .writeText(copyText)
-      .then(() => {
-        enableElement(element, textAlt);
-      })
-      .catch((err) => {
-        console.error('Failed to copy text: ', err);
-        enableElement(element, textAlt);
-      });
-  } else {
+/**
+ * Copy the Finsweet Attributes Script to the clipboard.
+ * @param element - trigger element
+ * @param scriptString - script string
+ * @param textToShow - text to show after copying
+ * @returns
+ */
+const copyScript = (scriptString: string | null, element: HTMLElement, textToShow = '') => {
+  if (!navigator.clipboard || !scriptString) {
     console.error('Clipboard API not available.');
+
+    return;
   }
+
+  const attributes = extractAttributes(scriptString);
+  disableElement(element);
+
+  const text = `<!-- Finsweet Attributes -->\n<script async type="module"${attributes}\n></script>`;
+
+  // copy
+  copy(text, element, textToShow);
+};
+
+/**
+ * Copy the Finsweet Attributes Text to the clipboard.
+ * @param content - attribute name or value
+ * @param element - trigger element
+ * @param textToShow - text to show after copying
+ * @returns
+ */
+const copyText = (content: string, element: HTMLElement, textToShow = '') => {
+  if (!navigator.clipboard || !content) {
+    console.error('Clipboard API not available.');
+
+    return;
+  }
+
+  disableElement(element);
+
+  // copy
+  copy(content.trim(), element, textToShow);
 };
 
 /**
  * Reformat a script tag string with each attribute on a new line using DOMParser.
- * @param {string} scriptString - The script tag as a string.
- * @returns {string} The reformatted script tag.
+ * @param scriptString - The script tag as a string.
+ * @returns The reformatted attributes.
  */
-const reformatScriptUsingDOMParser = (scriptString: string): string => {
+const extractAttributes = (scriptString: string): string => {
   const parser = new DOMParser();
+
   const doc = parser.parseFromString(scriptString, 'text/html');
   const scriptElement = doc.querySelector('script');
 
   if (!scriptElement) return '';
 
-  // Start constructing the reformatted string with the script tag and mandatory attributes
-  let reformattedScript = ``;
+  let attributes = ``;
 
-  // Then handle 'src' or any other attributes on new lines
+  // handle 'src' or any other attributes on new lines
   const otherAttributes = Array.from(scriptElement.attributes).filter((attr) => !['async', 'type'].includes(attr.name));
 
   for (const attr of otherAttributes) {
-    reformattedScript += `\n${attr.name}`;
+    attributes += `\n${attr.name}`;
     if (attr.value) {
-      reformattedScript += `="${attr.value}"`;
+      attributes += `="${attr.value.trim()}"`;
     }
   }
 
-  return reformattedScript;
+  return attributes;
 };
 
-export const initCopy = () => {
-  elements.forEach((element) => {
-    const copyButtons = document.querySelectorAll<HTMLElement>(element.target);
+/**
+ * Handle the copy event.
+ * @param e - The event object.
+ * @param textElement - The element to copy the text from.
+ * @param config - Element Configurations
+ * @param target - The target element.
+ * @returns
+ */
+const handleCopy = (e: Event, textElement: HTMLElement, config: Configurator, target: HTMLElement) => {
+  if (config.stopPropagation) {
+    e.stopPropagation();
+    e.stopImmediatePropagation();
+  }
 
-    if (copyButtons) {
-      copyButtons.forEach((copyButton) => {
-        // add default style copied-text
-        // Add class to show "Copied!"
-        copyButton.classList.add('copied-text');
+  if (config?.script) {
+    copyScript(textElement?.textContent, target, config?.changeText ? 'Copied!' : '');
 
-        let textElement: HTMLElement;
+    return;
+  }
 
-        if (element?.text && element?.button) {
-          // handle copy script for button
-          const parent = copyButton.parentElement;
-          if (element?.detached) {
-            textElement = document.querySelector(element.text) as HTMLElement;
-          } else {
-            textElement = parent?.querySelector(element.text) as HTMLElement;
-          }
-        } else if (element.text && !element?.button) {
-          // handle copy script for non-button, these are attributes name sand attribute values
-          const closest = copyButton.closest(element.text) as HTMLElement;
+  if (config.changeText) {
+    const text = textElement?.textContent || 'Failed to copy';
 
-          if (closest) {
-            textElement = closest;
-          } else {
-            const parent = copyButton.parentElement;
-            textElement = parent?.querySelector(element.text) as HTMLElement;
-          }
-        } else {
-          // handle copy script for non-button, these are attributes name sand attribute values
-          textElement = copyButton;
-        }
+    copyText(text, target, 'Copied!');
 
-        textElement.classList.add('copied-text');
+    return;
+  }
 
-        copyButton.addEventListener(
-          'click',
-          (e) => {
-            if (element.stopPropagation) {
-              e.stopPropagation();
-              e.stopImmediatePropagation();
-            }
+  const text = textElement?.textContent || 'Failed to copy';
 
-            if (element?.textContent) {
-              if (element?.format) {
-                const text = textElement?.textContent || 'Failed to copy';
-                // convert to DOM, then reconstruct the output property with Finsweet Attribute Heading
-                const formattedText = reformatScriptUsingDOMParser(text);
-                const changeText = element?.changeText;
-                copyToClipboard(reformatScriptTag(formattedText), copyButton, changeText ? 'Copied!' : '');
-              } else {
-                if (element.changeText) {
-                  const text = textElement?.textContent || 'Failed to copy';
-                  copyToClipboard(text, copyButton, 'Copied!', true);
-                } else {
-                  const text = textElement?.textContent || 'Failed to copy';
-                  copyToClipboard(text, textElement, '', true);
-                }
-              }
-            }
-          },
-          {
-            capture: element?.stopPropagation ? true : false,
-          }
-        );
-      });
+  copyText(text, textElement, '');
+};
+
+/**
+ * Initialize the copy functionality.
+ * @param config - The configuration object.
+ */
+export const initCopy = (config: Configurator) => {
+  const targetElements = document.querySelectorAll<HTMLElement>(config.target);
+
+  if (targetElements.length === 0) return;
+
+  targetElements.forEach((target) => {
+    // add default style copied-text
+    // Add class to show "Copied!"
+    target.classList.add('copied-text');
+
+    if (config.text && config.button) {
+      // handle copy script for button
+      const parent = target.parentElement;
+
+      let textElement = parent?.querySelector(config.text) as HTMLElement;
+
+      if (config?.detached) textElement = document.querySelector(config.text) as HTMLElement;
+
+      addClickEventListener(target, textElement, config);
+
+      return;
     }
+
+    if (config.text && !config?.button) {
+      // handle copy script for non-button, these are attributes name sand attribute values
+      const closest = target.closest(config.text) as HTMLElement;
+      const parent = target.parentElement;
+
+      let textElement = parent?.querySelector(config.text) as HTMLElement;
+
+      if (closest) textElement = closest;
+
+      addClickEventListener(target, textElement, config);
+
+      return;
+    }
+
+    // handle copy script for non-button, these are attributes names and attribute values
+    addClickEventListener(target, target, config);
   });
 };
 
+/**
+ * Add click event to the copy button.
+ * @param target - The target element to add the event listener to.
+ * @param textElement - The element to copy the text from.
+ * @param element - The copy element object.
+ */
+const addClickEventListener = (target: HTMLElement, textElement: HTMLElement, config: Configurator) => {
+  textElement.classList.add('copied-text');
+
+  target.addEventListener('click', (e) => handleCopy(e, textElement, config, target), {
+    capture: config?.stopPropagation ? true : false,
+  });
+};
+
+// DOM content loaded
 document.addEventListener('DOMContentLoaded', () => {
   // Inject the CSS into the <head>
   injectCSS(cssString);
 
-  initCopy();
+  configurator.forEach(initCopy);
 });
