b71f334939bb07f6b4f0f03343ccde3e96a44c9a
Alex Iglesias <alexiglesias@finsweet.com>
2025-06-17 21:12:09 +0200

    feat: Intl.NumberFormat options for fs-rangeslider-formatdisplay (#678)

--- packages/rangeslider/src/utils/constants.ts
+++ packages/rangeslider/src/utils/constants.ts
@@ -57,14 +57,6 @@ export const SETTINGS = {
   step: { key: 'step' },
 
   /**
-   * Defines if the Handles' value display should be formatted.
-   */
-  formatdisplay: {
-    key: 'formatdisplay',
-    values: ['true'],
-  },
-
-  /**
    * Defines when should the <input> elements be updated.
    */
   update: {
@@ -80,6 +72,38 @@ export const SETTINGS = {
     key: 'lazy',
     values: ['true'],
   },
+
+  /**
+   * Defines if the Handles' value display should be formatted.
+   */
+  formatdisplay: {
+    key: 'formatdisplay',
+    values: ['true'],
+  },
+
+  /**
+   * Defines the format options of the Handles' values.
+   */
+  formatcompactdisplay: { key: 'formatcompactdisplay' },
+  formatcurrency: { key: 'formatcurrency' },
+  formatcurrencydisplay: { key: 'formatcurrencydisplay' },
+  formatcurrencysign: { key: 'formatcurrencysign' },
+  formatmaximumsignificantdigits: { key: 'formatmaximumsignificantdigits', isNumeric: true },
+  formatmaximumfractiondigits: { key: 'formatmaximumfractiondigits', isNumeric: true },
+  formatminimumfractiondigits: { key: 'formatminimumfractiondigits', isNumeric: true },
+  formatminimumintegerdigits: { key: 'formatminimumintegerdigits', isNumeric: true },
+  formatminimumsignificantdigits: { key: 'formatminimumsignificantdigits', isNumeric: true },
+  formatnotation: { key: 'formatnotation' },
+  formatnumberingsystem: { key: 'formatnumberingsystem' },
+  formatroundingpriority: { key: 'formatroundingpriority' },
+  formatroundingincrement: { key: 'formatroundingincrement', isNumeric: true },
+  formatroundingmode: { key: 'formatroundingmode' },
+  formatsigndisplay: { key: 'formatsigndisplay' },
+  formatstyle: { key: 'formatstyle' },
+  formattrailingzerodisplay: { key: 'formattrailingzerodisplay' },
+  formatunit: { key: 'formatunit' },
+  formatunitdisplay: { key: 'formatunitdisplay' },
+  formatusegrouping: { key: 'formatusegrouping' },
 } as const satisfies AttributeSettings;
 
 export const HANDLE_INCREMENT_KEYS = [ARROW_UP_KEY, ARROW_RIGHT_KEY];
