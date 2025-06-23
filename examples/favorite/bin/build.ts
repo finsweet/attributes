65e4cb204f63893b5fb8bf7edabb3498c642f1dd
Alex Iglesias <alexiglesias@finsweet.com>
2025-06-23 20:54:23 +0200

    chore: dev script

--- examples/favorite/bin/build.ts
+++ examples/favorite/bin/build.ts
@@ -1,4 +1,8 @@
 import * as esbuild from 'esbuild';
+import http from 'http';
+
+const SERVE_PORT = 3003;
+const PROXY_PORT = 3002;
 
 // Create contexts
 const context = await esbuild.context({
@@ -7,5 +11,35 @@ const context = await esbuild.context({
   outdir: './dist',
 });
 
-await context.rebuild();
-context.dispose();
+await context.watch();
+const { hosts, port } = await context.serve({
+  servedir: './dist',
+  port: SERVE_PORT,
+});
+
+http
+  .createServer((req, res) => {
+    const { url, method, headers } = req;
+
+    const proxyReq = http.request(
+      {
+        port,
+        method,
+        headers,
+        path: url,
+        hostname: hosts[0],
+      },
+      (proxyRes) => {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.writeHead(proxyRes.statusCode!, proxyRes.headers);
+
+        proxyRes.pipe(res, { end: true });
+      }
+    );
+
+    req.pipe(proxyReq, { end: true });
+  })
+  .listen(PROXY_PORT);
+
+console.log(`Serving:`);
+console.log(`<script async src="http://localhost:${PROXY_PORT}/index.js"></script>`);
